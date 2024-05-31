9e1f5ea4f45ba9d99329244746e4369beac4617d
dinujpranesh <167699866+dinujpranesh@users.noreply.github.com>
2024-05-31 12:57:11 +0530

    new file:   .gitattributes 	new file:   README.md 	new file:   Register.html 	new file:   home.html 	new file:   img/card/011.png 	new file:   img/card/1.png 	new file:   img/card/2.png 	new file:   img/card/Employee Self-Service Portal.png 	new file:   img/dbregister.php 	new file:   img/fav/about.txt 	new file:   img/fav/android-chrome-192x192.png 	new file:   img/fav/android-chrome-512x512.png 	new file:   img/fav/apple-touch-icon.png 	new file:   img/fav/favicon-16x16.png 	new file:   img/fav/favicon-32x32.png 	new file:   img/fav/favicon.ico 	new file:   img/fav/favicon_io.zip 	new file:   img/fav/site.webmanifest 	new file:   img/home/Untitled design (1).png 	new file:   img/home/Untitled design (2).png 	new file:   img/home/Untitled design (3).png 	new file:   login.html 	new file:   site.webmanifest

---
+++ img/dbregister.php
@@ -0,0 +1,37 @@
+<?php
+// Connect to the database
+$servername = "your_servername";
+$username = "root";
+$password = "";
+$dbname = "AIphp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the submitted data
+$email = $_POST['email'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$phone = $_POST['phone'];
+$salary = $_POST['salary'];
+$gender = $_POST['gender'];
+$dateOfBirth = $_POST['dateOfBirth'];
+$password = $_POST['password'];
+$confirmPassword = $_POST['confirmPassword'];
+
+// Insert the data into the database
+$sql = "INSERT INTO employee (email, firstName, lastName, phone, salary, gender, dateOfBirth, password, confirmPassword)
+        VALUES ('$email', '$firstName', '$lastName', '$phone', '$salary', '$gender', '$dateOfBirth', '$password', '$confirmPassword')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
