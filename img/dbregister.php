580f3b383fc4ee5192a7363633938e2cb5c758c9
dinujpranesh <167699866+dinujpranesh@users.noreply.github.com>
2024-05-31 17:01:20 +0530

    modified:   Register.html 	modified:   img/dbregister.php

--- img/dbregister.php
+++ img/dbregister.php
@@ -1,33 +1,15 @@
 <?php
-// Enable error reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Connect to the database
 $servername = "your_servername";
-$username = "root";
-$password = "";
-$dbname = "aiphp";
-
-// Generate a random password
-function generatePassword($length = 8) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $password = '';
-
-    for ($i = 0; $i < $length; $i++) {
-        $index = rand(0, strlen($characters) - 1);
-        $password .= $characters[$index];
-    }
-
-    return $password;
-}
+$username = "your_username";
+$password = "your_password";
+$dbname = "AIphp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " );
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // Get the submitted data
@@ -38,29 +20,17 @@ $phone = $_POST['phone'];
 $salary = $_POST['salary'];
 $gender = $_POST['gender'];
 $dateOfBirth = $_POST['dateOfBirth'];
-$password = generatePassword();
+$password = $_POST['password'];
 
 // Insert the data into the database
 $sql = "INSERT INTO employee (email, firstName, lastName, phone, salary, gender, dateOfBirth, password)
         VALUES ('$email', '$firstName', '$lastName', '$phone', '$salary', '$gender', '$dateOfBirth', '$password')";
-try {
+
 if ($conn->query($sql) === TRUE) {
     echo "Data inserted successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-} catch (mysqli_sql_exception $e) {
-    if (strpos($e->getMessage(), 'Duplicate entry') !== false) {
-        header('Location:register.php?error');
-        exit();
 
-    } else {
-        echo "Error: " . $e->getMessage();
-    }
-}
-
-
-// Close the database connection
 $conn->close();
-
 ?>
