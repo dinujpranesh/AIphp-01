9330d3744219a72440b7c4d84ab8260dabdd056d
dinujpranesh <167699866+dinujpranesh@users.noreply.github.com>
2024-05-31 16:28:29 +0530

    modified:   img/dbregister.php 	new file:   index.html

--- img/dbregister.php
+++ img/dbregister.php
@@ -1,9 +1,14 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Connect to the database
 $servername = "your_servername";
 $username = "root";
 $password = "";
-$dbname = "AIphp";
+$dbname = "aiphp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
